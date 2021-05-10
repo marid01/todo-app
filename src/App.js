ed4981c34f750b587024a29a03be3031e983ec65
Marid <maridshikhkerimov@gmail.com>
2021-05-11 01:10:42 +0300

    component List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import listSvg from './assets/img/list.svg'
+import List from "./components/List";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="todo">
+            <div className="todo__sidebar">
+                <List items={[
+                    {
+                        icon: <img src={listSvg} alt=""/>,
+                        name: 'Все задачи',
+                        active: true
+                    }
+                ]}
+                />
+                <List items={[
+                    {
+                        color: 'green',
+                        name: 'Покупки',
+                    },
+                    {
+                        color: 'blue',
+                        name: 'Фронтенд',
+                    },
+                    {
+                        color: 'pink',
+                        name: 'Фильмы и сериалы',
+                    }
+                ]}
+                />
+                <List items={[
+                    {
+                        className: "list__add-button",
+                        icon: (
+                            <svg width="12" height="12" viewBox="0 0 16 16" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round"
+                                  strokeLinejoin="round"/>
+                            <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round"
+                                  strokeLinejoin="round"/>
+                        </svg>)
+                        ,
+                        name: 'Добавить список',
+                    }
+                ]}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/List/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from "classnames";
+
+import './List.scss';
+
+function List({items}) {
+    return (
+        <ul className="list">
+            {
+                items.map((item, index) => (
+                    <li key={index} className={classNames(item.className, {'active': item.active})}>
+                        <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i> }</i>
+                        <span>{item.name}</span>
+                    </li>
+                ) )
+            }
+
+        </ul>
+    )
+
+}
+
+export default List
