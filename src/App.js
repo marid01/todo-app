9398fcde67e678a03744587645201b7d492f1d8a
Marid <maridshikhkerimov@gmail.com>
2021-05-11 02:19:58 +0300

    added AddButtonList.jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import listSvg from './assets/img/list.svg'
 import List from "./components/List";
+import AddListButton from "./components/AddButtonList";
 
 function App() {
     return (
@@ -29,22 +30,8 @@ function App() {
                     }
                 ]}
                 />
-                <List items={[
-                    {
-                        className: "list__add-button",
-                        icon: (
-                            <svg width="12" height="12" viewBox="0 0 16 16" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                            <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round"
-                                  strokeLinejoin="round"/>
-                            <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round"
-                                  strokeLinejoin="round"/>
-                        </svg>)
-                        ,
-                        name: 'Добавить список',
-                    }
-                ]}
-                />
+                <AddListButton/>
+
             </div>
         </div>
     );
